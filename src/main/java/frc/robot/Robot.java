6d57bf02b3a0fdb91e148d60cf34e79d6ae7cbe4
Abdullah Khaled <abdullah.khaled.542@k12.friscoisd.org>
2025-02-23 22:10:08 -0600

    Abdullah - built code for alt method

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -160,7 +160,8 @@ public class Robot extends LoggedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    SmartDashboard.putString("Buffer/RobotState", Constants.toString(RobotContainer.operatorBufferState));
+    SmartDashboard.putString(
+        "Buffer/RobotState", Constants.toString(RobotContainer.operatorBufferState));
     RobotContainer.buffer.periodic();
   }
 
