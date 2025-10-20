54deb2d20397b518f3376b8ec813282e6d4bbeb4
Zayan Hassan <n0tzayan2022@gmail.com>
2025-10-20 17:46:37 -0500

    Conversion to generic types 10/20/25

--- src/main/java/frc/robot/util/Graph.java
+++ src/main/java/frc/robot/util/Graph.java
@@ -3,27 +3,27 @@ package frc.robot.util;
 import frc.robot.subsystems.Superstructure;
 import java.util.*;
 
-public class Graph<T> {
-  HashMap<T, List<Pair<T>>> graph;
+public class Graph<T, A> {
+  HashMap<T, List<Pair<T, A>>> graph;
 
   public Graph(T[] arr) {
     graph = new HashMap<>();
     for (T t : arr) {
-      graph.put(t, new ArrayList<Pair<T>>());
+      graph.put(t, new ArrayList<Pair<T, A>>());
     }
   }
 
-  public void add(T startState, T endState, double weight) {
+  public void add(T startState, T endState, A weight) {
     if (graph.containsKey(startState)) {
-      List<Pair<T>> edges = graph.get(startState);
-      edges.add(new Pair<T>(endState, weight));
+      List<Pair<T, A>> edges = graph.get(startState);
+      edges.add(new Pair<T, A>(endState, weight));
       graph.put(startState, edges);
     } else {
       throw new IllegalArgumentException();
     }
   }
 
-  public void addBoth(T startState, T endState, double transitionCost) {
+  public void addBoth(T startState, T endState, A transitionCost) {
     if (graph.containsKey(startState) && graph.containsKey(endState)) {
       add(startState, endState, transitionCost);
       add(endState, startState, transitionCost);
@@ -32,7 +32,7 @@ public class Graph<T> {
     }
   }
 
-  public List<Pair<T>> getAdj(Superstructure.State state) {
+  public List<Pair<T, A>> getAdj(Superstructure.State state) {
     return graph.get(state);
   }
 }
--- src/main/java/frc/robot/util/Pair.java
+++ src/main/java/frc/robot/util/Pair.java
@@ -2,11 +2,11 @@ package frc.robot.util;
 
 import frc.robot.subsystems.*;
 
-public class Pair<T> {
+public class Pair<T, A> {
   public T state;
-  public double cost;
+  public A cost;
 
-  public Pair(T state, double cost) {
+  public Pair(T state, A cost) {
     this.state = state;
     this.cost = cost;
   }
